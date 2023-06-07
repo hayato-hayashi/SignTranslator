dad8db2a04f7f27561aaf185c5de7a0c6db664ef
hayato <hayashi.hyt2001@icloud.com>
2023-06-07 11:31:23 +0900

    create_new_project

---
+++ app/src/main/java/jp/ac/thers/s/hayshi/signlanguagetranslator/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.thers.s.hayshi.signlanguagetranslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
